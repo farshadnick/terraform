f061a1b817a9d4f188af1670f0821a212f3fd2bd
Farshad Nick <88557305+farshadnick@users.noreply.github.com>
2022-05-11 00:27:22 +0430

    Update terraform installation.sh

--- terraform installation.sh
+++ terraform installation.sh
@@ -1,5 +1,5 @@
 mkdir -p /opt/terraform && cd /opt/terraform
 wget https://releases.hashicorp.com/terraform/1.1.9/terraform_1.1.9_linux_amd64.zip
-apt-get install unzip -y  && unzip terraform_0.15.3_linux_amd64.zip
+apt-get install unzip -y  && unzip terraform_1.1.9_linux_amd64.zip
 mv /opt/terraform/terraform /usr/bin/
-terraform -version
+terraform --version
